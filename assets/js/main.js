32cfc1d7f006887dce2afcbabb034aae0d8e1b5d
Alexandre Bastos <xande.bastos321@gmail.com>
2022-08-26 17:09:43 -0300

    feita a função de encriptação e descriptação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-
-
 /*
 `A letra "e" é convertida para "enter"`
 `A letra "i" é convertida para "imes"`
@@ -7,54 +5,71 @@
 `A letra "o" é convertida para "ober"`
 `A letra "u" é convertida para "ufat"`
 */
-const copiar = () => {}
 
+function encrypt(msg) {
+    let novaMsg = msg
+    .split(/e/igm).join("enter")
+    .split(/i/igm).join("imes")
+    .split(/a/igm).join("ai")
+    .split(/o/igm).join("ober")
+    .split(/u/igm).join("ufat")
+
+    return novaMsg
+}
+
+function decrypt(msg) {
+    novaMsg = msg
+    .replace(/enter/igm, "e")
+    .replace(/imes/igm, "i")
+    .replace(/ai/igm, "a")
+    .replace(/ober/igm, "o")
+    .replace(/ufat/igm, "u")
+
+    return novaMsg
+}
+
+$("#copy").click( () => {
+    const result = $("#result")
+    navigator.clipboard.writeText(result.val())
+            .then(()=> {
+                alert("texto copiado")
+            })
+})
 
 $("#criptografar").click( () => {
+    let encryptText
     const inputText = $("#text")
     const text = inputText.val()
     const result = $("#result")
     const btnCopy = $("#copy")
 
-    
     if(text != '') {
         $(".remove").remove()
 
         result.show();
         btnCopy.show()
 
-        result.val(inputText.val())
+        encryptText = encrypt(text)
+        result.val(encryptText)
         inputText.val('')
     }
-    
-    
-    const copy = $("#copy").click( () => {
-        navigator.clipboard.writeText(result.val())
-            .then(()=> {
-                alert("texto copiado")
-            })
-    })
 })
 
+$("#descriptografar").click( () => {
+    let decryptText
+    const inputText = $("#text")
+    const text = inputText.val()
+    const result = $("#result")
+    const btnCopy = $("#copy")
 
+    if(text != '') {
+        $(".remove").remove()
 
+        result.show();
+        btnCopy.show()
 
-
-
-const dicionario = [
-    ["e", "enter"],
-    ["i", "imes"],
-    ["a", "ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
-];
-
-let msg = 'xande bastos'
-    let novaMsg = 
-    msg
-    .split(/e/igm).join("enter")
-    .split(/i/igm).join("imes")
-    .split(/a/igm).join("ai")
-    .split(/o/igm).join("ober")
-    .split(/u/igm).join("ufat")
-    console.log(novaMsg)
+        decryptText = decrypt(text)
+        result.val(decryptText)
+        inputText.val('')
+    }
+})
