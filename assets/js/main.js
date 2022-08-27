204015919e3d37bc69061f5aabd06c2a0c9dfd49
Alexandre Bastos <xande.bastos321@gmail.com>
2022-08-27 12:40:33 -0300

    corrigindo erro da cor da fonte

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,71 +5,74 @@
 `A letra "o" é convertida para "ober"`
 `A letra "u" é convertida para "ufat"`
 */
-
 function encrypt(msg) {
     let novaMsg = msg
-    .split(/e/igm).join("enter")
-    .split(/i/igm).join("imes")
-    .split(/a/igm).join("ai")
-    .split(/o/igm).join("ober")
-    .split(/u/igm).join("ufat")
+        .split(/e/gim)
+        .join("enter")
+        .split(/i/gim)
+        .join("imes")
+        .split(/a/gim)
+        .join("ai")
+        .split(/o/gim)
+        .join("ober")
+        .split(/u/gim)
+        .join("ufat");
 
-    return novaMsg
+    return novaMsg;
 }
 
 function decrypt(msg) {
     novaMsg = msg
-    .replace(/enter/igm, "e")
-    .replace(/imes/igm, "i")
-    .replace(/ai/igm, "a")
-    .replace(/ober/igm, "o")
-    .replace(/ufat/igm, "u")
+        .replace(/enter/gim, "e")
+        .replace(/imes/gim, "i")
+        .replace(/ai/gim, "a")
+        .replace(/ober/gim, "o")
+        .replace(/ufat/gim, "u");
 
-    return novaMsg
+    return novaMsg;
 }
 
-$("#copy").click( () => {
-    const result = $("#result")
-    navigator.clipboard.writeText(result.val())
-            .then(()=> {
-                alert("texto copiado")
-            })
-})
+$("#copy").click(() => {
+    const result = $("#result");
+    navigator.clipboard.writeText(result.val()).then(() => {
+        alert("texto copiado");
+    });
+});
 
-$("#criptografar").click( () => {
-    let encryptText
-    const inputText = $("#text")
-    const text = inputText.val()
-    const result = $("#result")
-    const btnCopy = $("#copy")
+$("#criptografar").click(() => {
+    let encryptText;
+    const inputText = $("#text");
+    const text = inputText.val();
+    const result = $("#result");
+    const btnCopy = $("#copy");
 
-    if(text != '') {
-        $(".remove").remove()
-
-        result.show();
-        btnCopy.show()
-
-        encryptText = encrypt(text)
-        result.val(encryptText)
-        inputText.val('')
-    }
-})
-
-$("#descriptografar").click( () => {
-    let decryptText
-    const inputText = $("#text")
-    const text = inputText.val()
-    const result = $("#result")
-    const btnCopy = $("#copy")
-
-    if(text != '') {
-        $(".remove").remove()
+    if (text != "") {
+        $(".remove").remove();
 
         result.show();
-        btnCopy.show()
+        btnCopy.show();
 
-        decryptText = decrypt(text)
-        result.val(decryptText)
-        inputText.val('')
+        encryptText = encrypt(text);
+        result.val(encryptText);
+        inputText.val("");
     }
-})
+});
+
+$("#descriptografar").click(() => {
+    let decryptText;
+    const inputText = $("#text");
+    const text = inputText.val();
+    const result = $("#result");
+    const btnCopy = $("#copy");
+
+    if (text != "") {
+        $(".remove").remove();
+
+        result.show();
+        btnCopy.show();
+
+        decryptText = decrypt(text);
+        result.val(decryptText);
+        inputText.val("");
+    }
+});
